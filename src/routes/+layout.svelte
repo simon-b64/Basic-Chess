c462aa09e48dab8fc3f171bcf5ab169b5d1999bd
Simon B. <47189894+simon-b64@users.noreply.github.com>
2024-11-26 17:33:35 +0100

    feat: Removed template, configured prettier and added footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,57 +1,63 @@
 <script lang="ts">
-	import Header from './Header.svelte';
-	import '../app.css';
+    import githubLogoSvg from '$lib/images/github.svg';
+    import '../app.css';
 
-	let { children } = $props();
+    let { children } = $props();
 </script>
 
 <div class="app">
-	<Header />
+    <main>
+        {@render children()}
+    </main>
 
-	<main>
-		{@render children()}
-	</main>
-
-	<footer>
-		<p>
-			visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to learn about SvelteKit
-		</p>
-	</footer>
+    <footer>
+        <p>
+            Made with Svelte | Made by <a href="https://github.com/simon-b64">Simon B.<img src={githubLogoSvg} alt="GitHub" /></a>
+        </p>
+    </footer>
 </div>
 
 <style>
-	.app {
-		display: flex;
-		flex-direction: column;
-		min-height: 100vh;
-	}
+    .app {
+        display: flex;
+        flex-direction: column;
+        min-height: 100vh;
+    }
 
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 64rem;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
+    main {
+        display: flex;
+        flex-grow: 1;
+        flex-direction: column;
+        padding: 1rem;
+        width: 100%;
+        max-width: 64rem;
+        margin: 0 auto;
+        box-sizing: border-box;
+    }
 
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
+    footer {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        padding: 12px;
+    }
 
-	footer a {
-		font-weight: bold;
-	}
+    footer a {
+        font-weight: bold;
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+        gap: 0.2rem;
+    }
 
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
-	}
+    footer a img {
+        height: 1.5rem;
+    }
+
+    @media (min-width: 480px) {
+        footer {
+            padding: 12px 0;
+        }
+    }
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,59 +1,18 @@
-<script lang="ts">
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcomeFallback from '$lib/images/svelte-welcome.png';
-</script>
-
-<svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
-</svelte:head>
+<script lang="ts"></script>
 
 <section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcomeFallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
+    <h1>Basic - Chess</h1>
 </section>
 
 <style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
+    section {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
 
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
+    h1 {
+        width: 100%;
+    }
 </style>
